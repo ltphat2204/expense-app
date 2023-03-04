a45aa039ddd0d43954384cb7d84ad0d6a5874048
Yukirelia <ltphat2204@gmail.com>
2023-03-04 21:14:29 +0700

    finish add new

--- src/App.js
+++ src/App.js
@@ -1,30 +1,55 @@
 import { useState } from 'react';
 import Filter from './components/Filter';
 import Payment from './components/Payment';
+import Form from './components/Form';
 import './App.css';
 
 function App() {
   const listYear = ['2021', '2022', '2023', '2024'];
+  const listMonth = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   const [currentYear, setCurrentYear] = useState('2021');
+
+  const [isAdding, setAddingStatus] = useState(false);
+
   const [payment, setPayment] = useState([
     {
-      date: {year: 2021, month: 'Jan', day: 23},
+      date: {year: '2021', month: 'Jan', day: '23'},
       title: "Some drinks",
       amount: 100000
     },
     {
-      date: {year: 2022, month: 'Feb', day: 2},
+      date: {year: '2022', month: 'Feb', day: '2'},
       title: "Some books",
       amount: 50000
     }
   ]);
 
+  const handleAddPayment = (value) =>{
+    const [y, m, d] = value.date.split('-');
+
+    setPayment([
+      ...payment,
+      {
+        date: {year: y, month: listMonth[parseInt(m)], day: d},
+        title: value.name,
+        amount: value.amount
+      }
+    ]);
+  }
+
   return (
     <div className="App">
+      <div className="add_container">
+        {
+          isAdding ?
+          <Form onAdding={handleAddPayment} onCanceling={()=>setAddingStatus(false)}/> : 
+          <div onClick={()=>setAddingStatus(true)} className="add_button">ADD NEW EXPENSE</div>
+        }
+      </div>
       <div className="main_container">
         <Filter listOption={listYear} onChange={setCurrentYear} currentValue={currentYear} title="Filter by year"/>
         {
-          payment.filter((value) => value.date.year === parseInt(currentYear))
+          payment.filter((value) => value.date.year === currentYear)
                 .map((value, index)=><Payment key={index} data={value}/>)
         }
       </div>
---
+++ src/components/Form.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./Form.css";
+
+export default function Form({onAdding, onCanceling}){
+    const [data, setData] = useState({"name": "", "amount": "", "date": ""});
+
+    const handleChange = (event) => {
+        setData({
+            ...data,
+            [event.target.name]: event.target.value
+        });
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setData({"name": "", "amount": "", "date": ""});
+        onAdding(data);
+    } 
+    return (
+        <form className="add_form" onSubmit={handleSubmit}>
+            <div className="input_wrap">
+                <label htmlFor="name">Name: </label>
+                <input placeholder="Enter name here" id="name" name="name" value={data.name} onChange={handleChange}/>
+            </div>
+            <div className="input_wrap">
+                <label htmlFor="amount">Amount: </label>
+                <input placeholder="Enter amount here" id="amount" name="amount" value={data.amount} onChange={handleChange}/>
+            </div>
+            <div className="input_wrap">
+                <label htmlFor="date">Date: </label>
+                <input type="date" id="date" name="date" value={data.date} onChange={handleChange}/>
+            </div>
+            <div className="form_action_wrap">
+                <button type="submit">ADD</button>
+                <button onClick={onCanceling} type="cancel">CANCEL</button>
+            </div>
+        </form>
+    );
+}
